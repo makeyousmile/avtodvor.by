2c151453449934cd361b828b33a80762ea044570
sp3000 <denis.zarubin@gmail.com>
2026-01-20 15:08:23 +0300

    http server

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fs := http.FileServer(http.Dir("html"))
+	http.Handle("/", fs)
+
+	addr := ":" + port
+	log.Printf("Serving ./html on http://localhost%s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
